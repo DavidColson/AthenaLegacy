6ed06986c14efeffe41273cd044a06eb1d6c9572
DESKTOP-5ME1J2H\David <david@peripherallabs.com>
2018-10-23 23:36:00 +0100

    I forgot to submit a file :/

--- Source/main.cpp
+++ Source/main.cpp
@@ -14,6 +14,7 @@ struct CTransform
 {
 	vec3 m_pos;
 	float m_rot;
+	vec3 m_sca{ vec3(1.f, 1.f, 1.f) };
 };
 
 struct CSimpleRotate
@@ -83,18 +84,10 @@ public:
 		CTransform* pTransform = space->GetComponent<CTransform>(id);
 		CDrawable* pDrawable = space->GetComponent<CDrawable>(id);
 
-		// Create a render proxy for this entity and submit it
-		pDrawable->m_renderProxy = RenderProxy(
-			{
-				Vertex(vec3(0.0f, 0.0f, 0.5f), color(1.0f, 0.0f, 0.0f)),
-				Vertex(vec3(150.f, 150.f, 0.5f), color(0.0f, 1.0f, 0.0f)),
-				Vertex(vec3(300.f, 0.f, 0.5f), color(0.0f, 0.0f, 1.0f))
-			}, {
-				0, 1, 2, 0
-			});
+		assert(pDrawable->m_renderProxy.m_pWVPBuffer != nullptr);
 		Graphics::SubmitProxy(&pDrawable->m_renderProxy);
 
-		pDrawable->m_renderProxy.SetTransform(pTransform->m_pos, pTransform->m_rot);
+		pDrawable->m_renderProxy.SetTransform(pTransform->m_pos, pTransform->m_rot, pTransform->m_sca);
 	}
 
 	virtual void UpdateEntity(EntityID id, Space* space) override
@@ -102,7 +95,7 @@ public:
 		CTransform* pTransform = space->GetComponent<CTransform>(id);
 		CDrawable* pDrawable = space->GetComponent<CDrawable>(id);
 		
-		pDrawable->m_renderProxy.SetTransform(pTransform->m_pos, pTransform->m_rot);
+		pDrawable->m_renderProxy.SetTransform(pTransform->m_pos, pTransform->m_rot, pTransform->m_sca);
 	}
 
 	virtual void SetSubscriptions() override
@@ -119,8 +112,8 @@ int main(int argc, char *argv[])
 
 	SDL_Init(SDL_INIT_VIDEO);
 
-	float width = 640.0f;
-	float height = 480.0f;
+	float width = 1000.0f;
+	float height = 600.0f;
 
 	SDL_Window *window = SDL_CreateWindow(
 		"DirectX",
@@ -150,10 +143,41 @@ int main(int argc, char *argv[])
 	currentSpace->RegisterSystem<SDrawPolygon>();
 	currentSpace->RegisterSystem<SMovement>();
 
-	EntityID triangle2 = currentSpace->NewEntity();
-	currentSpace->AssignComponent<CTransform>(triangle2)->m_pos = vec3(100.0f, 100.0f, 0.0f);
-	currentSpace->AssignComponent<CDrawable>(triangle2);
-	currentSpace->AssignComponent<CPlayerControl>(triangle2);
+	EntityID asteroid = currentSpace->NewEntity();
+	currentSpace->AssignComponent<CTransform>(asteroid)->m_pos = vec3(100.0f, 100.0f, 0.0f);
+	currentSpace->AssignComponent<CDrawable>(asteroid)->m_renderProxy = RenderProxy(
+		{
+			Vertex(vec3(49.6f, 90.6f, 0.5f), color(1.0f, 0.0f, 0.0f)),
+			Vertex(vec3(39.f, 51.6f, 0.5f), color(0.0f, 1.0f, 0.0f)),
+			Vertex(vec3(19.6f, 54.2f, 0.5f), color(0.0f, 0.0f, 1.0f)),
+			Vertex(vec3(5.2f, 31.7f, 0.5f), color(0.0f, 0.0f, 1.0f)),
+			Vertex(vec3(24.2f, 8.3f, 0.5f), color(0.0f, 0.0f, 1.0f)),
+			Vertex(vec3(51.f, 20.f, 0.5f), color(0.0f, 0.0f, 1.0f)),
+			Vertex(vec3(97.f, 18.7f, 0.5f), color(0.0f, 0.0f, 1.0f)),
+			Vertex(vec3(75.3f, 45.f, 0.5f), color(0.0f, 0.0f, 1.0f)),
+			Vertex(vec3(95.f, 76.4f, 0.5f), color(0.0f, 0.0f, 1.0f)),
+		}, {
+			0, 1, 2, 3, 4, 5, 6, 7, 8, 0
+		});
+
+
+	EntityID ship = currentSpace->NewEntity();
+	CTransform* pTransform = currentSpace->AssignComponent<CTransform>(ship);
+	
+	pTransform->m_pos = vec3(500.0f, 300.0f, 0.0f);
+	pTransform->m_sca = vec3(0.3f, 0.3f, 1.0f);
+	
+	currentSpace->AssignComponent<CPlayerControl>(ship);
+	currentSpace->AssignComponent<CDrawable>(ship)->m_renderProxy = RenderProxy(
+		{
+			Vertex(vec3(0.f, 50.f, 0.5f), color(1.0f, 0.0f, 0.0f)),
+			Vertex(vec3(100.f, 80.f, 0.5f), color(0.0f, 1.0f, 0.0f)),
+			Vertex(vec3(85.f, 70.f, 0.5f), color(0.0f, 0.0f, 1.0f)),
+			Vertex(vec3(85.f, 30.f, 0.5f), color(0.0f, 0.0f, 1.0f)),
+			Vertex(vec3(100.f, 20.f, 0.5f), color(0.0f, 0.0f, 1.0f)),
+		}, {
+			0, 1, 2, 3, 4, 0
+		});
 
 
 
