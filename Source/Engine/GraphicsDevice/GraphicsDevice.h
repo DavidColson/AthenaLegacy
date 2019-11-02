bf8fcb2040894474c8a271f2ae614026b189f8a5
DESKTOP-5ME1J2H\David <davidcncolson@gmail.com>
2019-11-02 15:39:21 +0000

    Made indentation consistently tabs

--- Source/Engine/GraphicsDevice/GraphicsDevice.h
+++ Source/Engine/GraphicsDevice/GraphicsDevice.h
@@ -14,20 +14,20 @@ struct Context;
 
 enum class TopologyType
 {
-  TriangleStrip,
-  TriangleList,
-  LineStrip,
-  LineList,
-  PointList,
-  TriangleStripAdjacency,
-  TriangleListAdjacency,
-  LineStripAdjacency,
-  LineListAdjacency,
+	TriangleStrip,
+	TriangleList,
+	LineStrip,
+	LineList,
+	PointList,
+	TriangleStripAdjacency,
+	TriangleListAdjacency,
+	LineStripAdjacency,
+	LineListAdjacency,
 };
 
 #define DEFINE_HANDLE(_name)                                                          \
-  struct _name { uint16_t id{ UINT16_MAX }; };                                        \
-  inline bool IsValid(_name _handle) { return UINT16_MAX != _handle.id; }
+	struct _name { uint16_t id{ UINT16_MAX }; };                                        \
+	inline bool IsValid(_name _handle) { return UINT16_MAX != _handle.id; }
 
 #define INVALID_HANDLE { UINT16_MAX }
 
@@ -44,185 +44,185 @@ DEFINE_HANDLE(ConstBufferHandle)
 
 enum class Filter
 {
-  Linear,
-  Point,
-  Anisotropic
+	Linear,
+	Point,
+	Anisotropic
 };
 
 enum class WrapMode
 {
-  Wrap,
-  Mirror,
-  Clamp,
-  Border
+	Wrap,
+	Mirror,
+	Clamp,
+	Border
 };
 
 enum class ShaderType
 {
-  Vertex,
-  Pixel,
-  Geometry
+	Vertex,
+	Pixel,
+	Geometry
 };
 
 enum class AttributeType
 {
-  // Need to fill this out
-  float3,
-  float2
+	// Need to fill this out
+	float3,
+	float2
 };
 
 enum class TextureFormat
 {
-  // Should probably fill this out, but this'll do for now
-  RGBA32F,
-  R8,
-  D24S8
+	// Should probably fill this out, but this'll do for now
+	RGBA32F,
+	R8,
+	D24S8
 };
 
 enum class BlendOp
 {
-  Add,
-  Subtract,
-  RevSubtract,
-  Min,
-  Max
+	Add,
+	Subtract,
+	RevSubtract,
+	Min,
+	Max
 };
 
 enum class Blend
 {
-  Zero,
-  One,
-  SrcColor,
-  InverseSrcColor,
-  SrcAlpha,
-  InverseSrcAlpha,
-  DestAlpha,
-  InverseDestAlpha,
-  DestColor,
-  InverseDestColor,
-  BlendFactor,
-  InverseBlendFactor
+	Zero,
+	One,
+	SrcColor,
+	InverseSrcColor,
+	SrcAlpha,
+	InverseSrcAlpha,
+	DestAlpha,
+	InverseDestAlpha,
+	DestColor,
+	InverseDestColor,
+	BlendFactor,
+	InverseBlendFactor
 };
 
 struct VertexInputElement
 {
-  const char* name;
-  AttributeType type;
+	const char* name;
+	AttributeType type;
 };
 
 struct BlendingInfo
 {
-  bool enabled{ true };
-  BlendOp colorOp{ BlendOp::Add };
-  BlendOp alphaOp{ BlendOp::Add };
-  Blend source{ Blend::One };
-  Blend destination{ Blend::Zero };
-  Blend sourceAlpha{ Blend::One };
-  Blend destinationAlpha{ Blend::Zero };
-  std::array<float, 4> blendFactor;
+	bool enabled{ true };
+	BlendOp colorOp{ BlendOp::Add };
+	BlendOp alphaOp{ BlendOp::Add };
+	Blend source{ Blend::One };
+	Blend destination{ Blend::Zero };
+	Blend sourceAlpha{ Blend::One };
+	Blend destinationAlpha{ Blend::Zero };
+	std::array<float, 4> blendFactor;
 };
 
 namespace GfxDevice
 {
-  void Initialize(SDL_Window* pWindow, float width, float height);
+	void Initialize(SDL_Window* pWindow, float width, float height);
 
-  void PrintQueuedDebugMessages();
+	void PrintQueuedDebugMessages();
 
-  SDL_Window* GetWindow();
+	SDL_Window* GetWindow();
 
-  float GetWindowWidth();
+	float GetWindowWidth();
 
-  float GetWindowHeight();
+	float GetWindowHeight();
 
-  void SetViewport(float x, float y, float width, float height);
+	void SetViewport(float x, float y, float width, float height);
 
-  void ClearBackBuffer(std::array<float, 4> color);
+	void ClearBackBuffer(std::array<float, 4> color);
 
-  void SetTopologyType(TopologyType type);
+	void SetTopologyType(TopologyType type);
 
-  void SetBackBufferActive();
+	void SetBackBufferActive();
 
-  void PresentBackBuffer();
+	void PresentBackBuffer();
 
-  void ClearRenderState();
+	void ClearRenderState();
 
-  void DrawIndexed(int indexCount, int startIndex, int startVertex);
+	void DrawIndexed(int indexCount, int startIndex, int startVertex);
 
-  void Draw(int numVerts, int startVertex);
+	void Draw(int numVerts, int startVertex);
 
-  // Blend States
+	// Blend States
 
-  void SetBlending(const BlendingInfo& info);
+	void SetBlending(const BlendingInfo& info);
 
-  // Vertex Buffers
+	// Vertex Buffers
 
-  VertexBufferHandle CreateVertexBuffer(size_t numElements, size_t _elementSize, void* data);
+	VertexBufferHandle CreateVertexBuffer(size_t numElements, size_t _elementSize, void* data);
 
-  VertexBufferHandle CreateDynamicVertexBuffer(size_t numElements, size_t _elementSize);
+	VertexBufferHandle CreateDynamicVertexBuffer(size_t numElements, size_t _elementSize);
 
-  void UpdateDynamicVertexBuffer(VertexBufferHandle handle, void* data, size_t dataSize);
+	void UpdateDynamicVertexBuffer(VertexBufferHandle handle, void* data, size_t dataSize);
 
-  void BindVertexBuffer(VertexBufferHandle handle);
+	void BindVertexBuffer(VertexBufferHandle handle);
 
-  // Index Buffers
+	// Index Buffers
 
-  IndexBufferHandle CreateIndexBuffer(size_t numElements, void* data);
+	IndexBufferHandle CreateIndexBuffer(size_t numElements, void* data);
 
-  IndexBufferHandle CreateDynamicIndexBuffer(size_t numElements);
+	IndexBufferHandle CreateDynamicIndexBuffer(size_t numElements);
 
-  void UpdateDynamicIndexBuffer(IndexBufferHandle handle, void* data, size_t dataSize);
+	void UpdateDynamicIndexBuffer(IndexBufferHandle handle, void* data, size_t dataSize);
 
-  int GetIndexBufferSize(IndexBufferHandle handle);
+	int GetIndexBufferSize(IndexBufferHandle handle);
 
-  void BindIndexBuffer(IndexBufferHandle handle);
+	void BindIndexBuffer(IndexBufferHandle handle);
 
-  // Shaders And Programs
+	// Shaders And Programs
 
-  VertexShaderHandle CreateVertexShader(const wchar_t* fileName, const char* entry, const std::vector<VertexInputElement>& inputLayout);
+	VertexShaderHandle CreateVertexShader(const wchar_t* fileName, const char* entry, const std::vector<VertexInputElement>& inputLayout);
 
-  VertexShaderHandle CreateVertexShader(std::string& fileContents, const char* entry, const std::vector<VertexInputElement>& inputLayout);
+	VertexShaderHandle CreateVertexShader(std::string& fileContents, const char* entry, const std::vector<VertexInputElement>& inputLayout);
 
-  PixelShaderHandle CreatePixelShader(const wchar_t* fileName, const char* entry);
+	PixelShaderHandle CreatePixelShader(const wchar_t* fileName, const char* entry);
 
-  PixelShaderHandle CreatePixelShader(std::string& fileContents, const char* entry);
+	PixelShaderHandle CreatePixelShader(std::string& fileContents, const char* entry);
 
-  GeometryShaderHandle CreateGeometryShader(const wchar_t* fileName, const char* entry);
+	GeometryShaderHandle CreateGeometryShader(const wchar_t* fileName, const char* entry);
 
-  GeometryShaderHandle CreateGeometryShader(std::string& fileContents, const char* entry);
+	GeometryShaderHandle CreateGeometryShader(std::string& fileContents, const char* entry);
 
-  ProgramHandle CreateProgram(VertexShaderHandle vShader, PixelShaderHandle pShader);
+	ProgramHandle CreateProgram(VertexShaderHandle vShader, PixelShaderHandle pShader);
 
-  ProgramHandle CreateProgram(VertexShaderHandle vShader, PixelShaderHandle pShader, GeometryShaderHandle gShader);
+	ProgramHandle CreateProgram(VertexShaderHandle vShader, PixelShaderHandle pShader, GeometryShaderHandle gShader);
 
-  void BindProgram(ProgramHandle handle);
+	void BindProgram(ProgramHandle handle);
 
-  // Render Targets
+	// Render Targets
 
-  RenderTargetHandle CreateRenderTarget(float width, float height);
+	RenderTargetHandle CreateRenderTarget(float width, float height);
 
-  void BindRenderTarget(RenderTargetHandle handle);
+	void BindRenderTarget(RenderTargetHandle handle);
 
-  void UnbindRenderTarget(RenderTargetHandle handle);
+	void UnbindRenderTarget(RenderTargetHandle handle);
 
-  void ClearRenderTarget(RenderTargetHandle handle, std::array<float, 4> color, bool clearDepth, bool clearStencil);
+	void ClearRenderTarget(RenderTargetHandle handle, std::array<float, 4> color, bool clearDepth, bool clearStencil);
 
-  TextureHandle GetTexture(RenderTargetHandle handle);
+	TextureHandle GetTexture(RenderTargetHandle handle);
 
-  // Samplers
+	// Samplers
 
-  SamplerHandle CreateSampler(Filter filter = Filter::Linear, WrapMode wrapMode = WrapMode::Wrap);
+	SamplerHandle CreateSampler(Filter filter = Filter::Linear, WrapMode wrapMode = WrapMode::Wrap);
 
-  void BindSampler(SamplerHandle handle, ShaderType shader, int slot);
+	void BindSampler(SamplerHandle handle, ShaderType shader, int slot);
 
-  // Textures
+	// Textures
 
-  TextureHandle CreateTexture(int width, int height, TextureFormat format, void* data);
+	TextureHandle CreateTexture(int width, int height, TextureFormat format, void* data);
 
-  void BindTexture(TextureHandle, ShaderType shader, int slot);
+	void BindTexture(TextureHandle, ShaderType shader, int slot);
 
-  // Shader Constants
+	// Shader Constants
 
-  ConstBufferHandle CreateConstantBuffer(uint32_t bufferSize);
+	ConstBufferHandle CreateConstantBuffer(uint32_t bufferSize);
 
-  void BindConstantBuffer(ConstBufferHandle handle, const void* bufferData, ShaderType shader, int slot);
+	void BindConstantBuffer(ConstBufferHandle handle, const void* bufferData, ShaderType shader, int slot);
 }
--- Source/Engine/TypeSystem.h
+++ Source/Engine/TypeSystem.h
@@ -10,93 +10,93 @@ struct TypeData;
 
 struct Member
 {
-  const char* name;
-  size_t offset;
-  TypeData* pType;
+	const char* name;
+	size_t offset;
+	TypeData* pType;
 
-  template<typename T>
-  bool IsType()
-  {
-    // #TODO: add actual proper equality check not this hack 
-    return pType == &TypeDatabase::Get<T>(); 
-  }
+	template<typename T>
+	bool IsType()
+	{
+		// #TODO: add actual proper equality check not this hack 
+		return pType == &TypeDatabase::Get<T>(); 
+	}
 
-  TypeData& GetType()
-  {
-    return *pType;
-  }
+	TypeData& GetType()
+	{
+		return *pType;
+	}
 
-  template<typename T>
-  T* Get(void* instance)
-  {
-    return reinterpret_cast<T*>((char*)instance + offset);
-  }
-  template<typename T>
-  void Set(void* instance, T newValue)
-  {
-    *reinterpret_cast<T*>((char*)instance + offset) = newValue;
-  }
+	template<typename T>
+	T* Get(void* instance)
+	{
+		return reinterpret_cast<T*>((char*)instance + offset);
+	}
+	template<typename T>
+	void Set(void* instance, T newValue)
+	{
+		*reinterpret_cast<T*>((char*)instance + offset) = newValue;
+	}
 };
 
 struct TypeData
 {
-  const char* name;
-  size_t size;
-  std::map<std::string, Member> members;
+	const char* name;
+	size_t size;
+	std::map<std::string, Member> members;
 
-  TypeData(void(*initFunc)(TypeData*)) : TypeData{ nullptr, 0 }
-  {
-	  initFunc(this);
-  }
-  TypeData(const char* _name, size_t _size) : name(_name), size(_size) {}
-  TypeData(const char* _name, size_t _size, const std::initializer_list<std::pair<const std::string, Member>>& init) : name(_name), size(_size), members( init ) {}
+	TypeData(void(*initFunc)(TypeData*)) : TypeData{ nullptr, 0 }
+	{
+		initFunc(this);
+	}
+	TypeData(const char* _name, size_t _size) : name(_name), size(_size) {}
+	TypeData(const char* _name, size_t _size, const std::initializer_list<std::pair<const std::string, Member>>& init) : name(_name), size(_size), members( init ) {}
 
-  Member& GetMember(const char* _name);
+	Member& GetMember(const char* _name);
 
-   struct MemberIterator
-  {
-    MemberIterator(std::map<std::string, Member>::iterator _it) : it(_it) {}
+	 struct MemberIterator
+	{
+		MemberIterator(std::map<std::string, Member>::iterator _it) : it(_it) {}
 
-    Member& operator*() const 
-    { 
-      return it->second;
-    }
+		Member& operator*() const 
+		{ 
+			return it->second;
+		}
 
-    bool operator==(const MemberIterator& other) const 
-    {
-      return it == other.it;
-    }
-    bool operator!=(const MemberIterator& other) const 
-    {
-      return it != other.it;
-    }
+		bool operator==(const MemberIterator& other) const 
+		{
+			return it == other.it;
+		}
+		bool operator!=(const MemberIterator& other) const 
+		{
+			return it != other.it;
+		}
 
-    MemberIterator& operator++()
-    {
-      ++it;
-      return *this;
-    }
+		MemberIterator& operator++()
+		{
+			++it;
+			return *this;
+		}
 
-    std::map<std::string, Member>::iterator it;
-  };
+		std::map<std::string, Member>::iterator it;
+	};
 
-  const MemberIterator begin() 
-  {
-    return MemberIterator(members.begin());
-  }
+	const MemberIterator begin() 
+	{
+		return MemberIterator(members.begin());
+	}
 
-  const MemberIterator end()
-  {
-    return MemberIterator(members.end());
-  }
+	const MemberIterator end()
+	{
+		return MemberIterator(members.end());
+	}
 };
 
 // Type resolving mechanism
 template<typename T>
 TypeData& getPrimitiveTypeData() 
 {
-  static TypeData unknownType("UnkownType", 0); 
-  return unknownType;
+	static TypeData unknownType("UnkownType", 0); 
+	return unknownType;
 }
 
 template <>
@@ -112,53 +112,53 @@ TypeData& getPrimitiveTypeData<bool>();
 
 struct DefaultTypeResolver 
 {
-  // the decltype term here may result in invalid c++ if T doesn't contain typeData. 
-  // As such if it doesn't, this template instantiation will be completely ignored by the compiler through SFINAE. 
-  // If it does contain typeData, this will be instantiated and we're all good.
-  // See here for deep explanation: https://stackoverflow.com/questions/1005476/how-to-detect-whether-there-is-a-specific-member-variable-in-class
-  template<typename T, typename = int>
-  struct IsReflected : std::false_type {};
-  template<typename T>
-  struct IsReflected<T, decltype((void) T::typeData, 0)> : std::true_type {};
+	// the decltype term here may result in invalid c++ if T doesn't contain typeData. 
+	// As such if it doesn't, this template instantiation will be completely ignored by the compiler through SFINAE. 
+	// If it does contain typeData, this will be instantiated and we're all good.
+	// See here for deep explanation: https://stackoverflow.com/questions/1005476/how-to-detect-whether-there-is-a-specific-member-variable-in-class
+	template<typename T, typename = int>
+	struct IsReflected : std::false_type {};
+	template<typename T>
+	struct IsReflected<T, decltype((void) T::typeData, 0)> : std::true_type {};
 
-  // We're switching template versions depending on whether T has been internally reflected (i.e. has an typeData member)
-  template<typename T, typename std::enable_if<IsReflected<T>::value, int>::type = 0>
-  static TypeData& Get()
-  {
-    return T::typeData;
-  }
-  template<typename T, typename std::enable_if<!IsReflected<T>::value, int>::type = 0>
-  static TypeData& Get()
-  {
-    return getPrimitiveTypeData<T>();
-  }
+	// We're switching template versions depending on whether T has been internally reflected (i.e. has an typeData member)
+	template<typename T, typename std::enable_if<IsReflected<T>::value, int>::type = 0>
+	static TypeData& Get()
+	{
+		return T::typeData;
+	}
+	template<typename T, typename std::enable_if<!IsReflected<T>::value, int>::type = 0>
+	static TypeData& Get()
+	{
+		return getPrimitiveTypeData<T>();
+	}
 };
 
 
 namespace TypeDatabase
 {
-  // For storing lookup tables and other static type information
-  struct Data
-  {
-    static Data& Get() 
-    {
-      if (pInstance == nullptr)
-        pInstance = new Data();
-      return *pInstance;
-    }
-    static Data* pInstance;
+	// For storing lookup tables and other static type information
+	struct Data
+	{
+		static Data& Get() 
+		{
+			if (pInstance == nullptr)
+				pInstance = new Data();
+			return *pInstance;
+		}
+		static Data* pInstance;
 
-    std::unordered_map<std::string, TypeData*> typeNames;
-  };
+		std::unordered_map<std::string, TypeData*> typeNames;
+	};
 
-  TypeData& GetFromString(const char* name);
+	TypeData& GetFromString(const char* name);
 
-  // Generic typedata return
-  template<typename T>
-  TypeData& Get()
-  {
-    return DefaultTypeResolver::Get<T>();
-  }
+	// Generic typedata return
+	template<typename T>
+	TypeData& Get()
+	{
+		return DefaultTypeResolver::Get<T>();
+	}
 };
 
 
@@ -166,33 +166,33 @@ namespace TypeDatabase
 
 // Reflection macros
 #define REFLECT()                               \
-  static TypeData typeData;                \
-  static void initReflection(TypeData* type);
+	static TypeData typeData;                \
+	static void initReflection(TypeData* type);
 
 #define REFLECT_BEGIN(Struct)\
-  TypeData Struct::typeData{Struct::initReflection};\
-  void Struct::initReflection(TypeData* typeData) {\
-    using XX = Struct;\
-    TypeDatabase::Data::Get().typeNames.emplace(#Struct, typeData);\
-    typeData->name = #Struct;\
-    typeData->size = sizeof(XX);\
-    typeData->members = {
+	TypeData Struct::typeData{Struct::initReflection};\
+	void Struct::initReflection(TypeData* typeData) {\
+		using XX = Struct;\
+		TypeDatabase::Data::Get().typeNames.emplace(#Struct, typeData);\
+		typeData->name = #Struct;\
+		typeData->size = sizeof(XX);\
+		typeData->members = {
 
 #define REFLECT_MEMBER(member)\
-      {#member, {#member, offsetof(XX, member), &TypeDatabase::Get<decltype(XX::member)>()}},
+			{#member, {#member, offsetof(XX, member), &TypeDatabase::Get<decltype(XX::member)>()}},
 
 #define REFLECT_END()\
-    };\
-  }
+		};\
+	}
 
 
 // Special version of the begin macro for types that are template specializations, such as Vec<float>
 #define REFLECT_TEMPLATED_BEGIN(Struct)\
-  TypeData Struct::typeData{Struct::initReflection};\
-  template<>\
-  void Struct::initReflection(TypeData* typeData) {\
-    using XX = Struct;\
-    TypeDatabase::Data::Get().typeNames.emplace(#Struct, typeData);\
-    typeData->name = #Struct;\
-    typeData->size = sizeof(XX);\
-    typeData->members = {
+	TypeData Struct::typeData{Struct::initReflection};\
+	template<>\
+	void Struct::initReflection(TypeData* typeData) {\
+		using XX = Struct;\
+		TypeDatabase::Data::Get().typeNames.emplace(#Struct, typeData);\
+		typeData->name = #Struct;\
+		typeData->size = sizeof(XX);\
+		typeData->members = {
