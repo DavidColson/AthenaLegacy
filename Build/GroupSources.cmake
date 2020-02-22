e0b158011f28ec6cc6aa83c42101233acd58481c
DESKTOP-5ME1J2H\David <davidcncolson@gmail.com>
2020-02-22 13:53:15 +0000

    Removed copyright notice as we edited this beyond recognition.

--- Build/GroupSources.cmake
+++ Build/GroupSources.cmake
@@ -1,17 +1,5 @@
-# Copyright (C) 2008-2015 TrinityCore <http://www.trinitycore.org/>
-#
-# This file is free software; as a special exception the author gives
-# unlimited permission to copy and/or distribute it, with or without
-# modifications, as long as this notice is preserved.
-#
-# This program is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY, to the extent permitted by law; without even the
-# implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-
 macro(GroupSources dir)
-    # Include all header and c files
     file(GLOB_RECURSE sources RELATIVE ${dir} *.h *.hpp *.c *.cpp *.cc)
-
     foreach(source ${sources})
         get_filename_component(source_path "${source}" PATH)
         string(REPLACE "/" "\\" source_path_msvc "${source_path}")
