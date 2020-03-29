2c7c53e62db91b8a5295fa5fd80ee3f8c2dd9687
DESKTOP-5ME1J2H\David <davidcncolson@gmail.com>
2020-03-29 15:34:21 +0100

    Greatly improved logging window in game

--- Source/Engine/Modules/Editor/Editor.cpp
+++ Source/Engine/Modules/Editor/Editor.cpp
@@ -27,25 +27,62 @@ namespace {
 	double oldObservedFrameTime;
 }
 
+struct LogApp
+{
+    ImGuiTextFilter     filter;
+    bool                scrollToBottom{ true };
+
+    void Draw()
+    {
+        ImGui::Begin("Log", &showLog);
+
+		ImGui::Checkbox("Scroll To Bottom", &scrollToBottom);
+		ImGui::SameLine();
+		ImGui::Button("Clear");
+		ImGui::SameLine();
+		filter.Draw("Filter", -100.0f);
+		ImGui::Separator();
+
+		ImGui::BeginChild("ScrollingRegion", ImVec2(0, 0), false, ImGuiWindowFlags_HorizontalScrollbar);
+
+		for (const Log::LogEntry& entry : Log::GetLogHistory())
+		{
+			const char* item = entry.message.c_str();
+			if (filter.PassFilter(item, nullptr))
+			{
+				ImVec4 col = ImGui::GetStyleColorVec4(ImGuiCol_Text);;
+				switch (entry.level)
+				{
+				case Log::ECrit:
+					col = ImColor(1.0f, 0.0f, 0.0f); break;
+				case Log::EWarn:
+					col = ImColor(1.0f, 1.0f, 0.0f); break;
+				default: break;
+				}
+
+				ImGui::PushStyleColor(ImGuiCol_Text, col);
+				ImGui::TextUnformatted(item);
+            	ImGui::PopStyleColor();
+			}
+		}
+		
+		if (scrollToBottom)
+			ImGui::SetScrollHereY(1.0f);
+		ImGui::EndChild();
+
+		ImGui::End();
+    }
+};
+
 void ShowLog()
 {
+	// This isn't ideal, put it inside an editor state struct or something
+	static LogApp log;
+
 	if (!showLog)
 		return;
 
-	ImGui::Begin("Log", &showLog);
-
-	ImGui::BeginChild("ScrollingRegion", ImVec2(0, 0), false, ImGuiWindowFlags_HorizontalScrollbar);
-
-	for (const Log::LogEntry& entry : Log::GetLogHistory())
-	{
-		const char* item = entry.message.c_str();
-
-		ImGui::TextUnformatted(item);
-	}
-	ImGui::SetScrollHereY(1.0f);
-	ImGui::EndChild();
-
-	ImGui::End();
+	log.Draw();
 }
 
 void ShowEntityInspector(Scene& scene)
